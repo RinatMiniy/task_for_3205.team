28bd014fa9061b788ace5b9b42164bf146c6f85a
RinatMiniy <RinatMiniy@gmail.com>
2023-07-14 13:20:13 +0400

    feat: add search in json

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,53 @@
 const express = require("express")
+const fs = require('fs');
+const { body, validationResult } = require('express-validator');
 
 const PORT = 5000
 
 const app = express()
+app.use(express.json())
 
-const start = () => {
+app.post('/search', [
+  body('email').isEmail(),
+  body('number').optional().isInt(),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { email, number } = req.body;
+
   try {
-    app.listen(PORT, () => console.log("server start"))
-  } catch {
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    let data = {}
+
+    await fs.promises.readFile("./data.json", "utf-8")
+      .then(function (result) {
+          data = JSON.parse(result);
+      })
+      .catch(function (error) {
+          console.log(error);
+      })
+
+    const filteredUsers = data.data.filter((user) => {
+      console.log(user)
+        if (email && email !== user.email) {
+          return false;
+        }
+        if (number && number !== user.number.toString()) {
+          return false;
+        }
+        return true;
+      });
+
+
+    res.json(filteredUsers);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
 
   }
-}
+});
 
-start()
+app.listen(PORT, () => console.log("server start"))
